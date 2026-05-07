2f80cb4e134f3ce9332a1ae1a4bc82e9992e6bac
jamesfarrington1 <80847880+jamesfarrington1@users.noreply.github.com>
2026-05-07 01:55:43 -0400

    Started Project 6: Current Address

---
+++ p2/sketch.js
@@ -0,0 +1,311 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlYBlTNZEPGK0miEIuWeNLVREWIMB9ia8",
+  authDomain: "current-address-fa4d0.firebaseapp.com",
+  projectId: "current-address-fa4d0",
+  storageBucket: "current-address-fa4d0.firebasestorage.app",
+  messagingSenderId: "87716306783",
+  appId: "1:87716306783:web:e4ec83bdd9738ac9d320ea",
+  measurementId: "G-Q4RTF9WGS9"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+async function testFirebase() {
+  await addDoc(collection(db, "postcards"), {
+    message: "firebase is working",
+    createdAt: Date.now()
+  });
+
+  console.log("SAVED TO FIREBASE");
+}
+
+//testFirebase();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  let cardX;
+  let cardY;
+let cardW = 1000;
+let cardH = 600;
+
+let postcardImage;
+let postcardImageUrl = "";
+let postcardTitle = "";
+let currentPostcardId = null;
+
+let flipped = false;
+let doodleLayer;
+let drawing = false;
+
+let cardRotation = 0;
+let targetRotation = 0;
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  let button = createButton("New Postcard");
+
+  button.mousePressed(getRandomImage);
+
+  cardX = width / 2 - cardW / 2;
+  cardY = height / 2 - cardH / 2;
+
+  doodleLayer = createGraphics(cardW, cardH);
+  doodleLayer.background(255);
+
+  let flipButton = createButton("Flip Postcard");
+    
+    flipButton.mousePressed(() => {
+        if (targetRotation === 0) {
+            targetRotation = 180;
+            } else {
+            targetRotation = 0;
+            }
+        } );
+
+    let sendButton = createButton("Send Postcard Into the Void");
+    
+    sendButton.mousePressed(sendPostcard);
+
+    let receiveButton = createButton("Receive Postcard From the Void");
+
+    receiveButton.mousePressed(receivePostcard);
+
+
+
+
+        button.parent("toolbar");
+        flipButton.parent("toolbar");
+        sendButton.parent("toolbar");
+        receiveButton.parent("toolbar");
+}
+
+async function getRandomImage() {
+    const response = await fetch(
+        "https://commons.wikimedia.org/w/api.php?action=query&generator=random&grnnamespace=6&prop=imageinfo&iiprop=url&format=json&origin=*"
+    )
+
+    
+
+     const data = await response.json();
+
+     const page = Object.values(data.query.pages)[0];
+
+     postcardTitle = page.title;
+
+     postcardImageUrl = page.imageinfo[0].url;
+
+     loadImage(postcardImageUrl, img => {
+        postcardImage = img;
+        currentPostcardId = null;
+        cardRotation = 0;
+
+        doodleLayer.background(255);
+        flipped = false;
+        targetRotation = 0;
+     });
+
+    }
+
+function draw() {
+
+  background(220);
+
+  fill(0);
+
+  textSize(20);
+
+  //text(postcardTitle, 40, 100);
+
+ // if (postcardImage) {
+
+  // image(postcardImage, cardX, cardY, cardW, cardH);
+
+ // }
+
+  cardRotation = lerp(cardRotation, targetRotation, 0.12);
+
+  if (cardRotation > 90) {
+    flipped = true;
+  } else {
+    flipped = false;
+  }
+
+  let squeeze = abs(cos(radians(cardRotation)));
+
+
+push();
+translate(cardX + cardW / 2, cardY + cardH / 2);
+scale(squeeze, 1);
+
+
+
+        if (flipped && postcardImage) {
+            strokeWeight(6);
+            fill(255);
+            rect(-cardW / 2, -cardH / 2, cardW, cardH);
+
+            image(doodleLayer, -cardW / 2, -cardH / 2);
+
+           
+
+
+            fill(0);
+            textSize(10);
+            text("Draw your message here!", -cardW / 2 + 10, -cardH / 2 + 20);
+
+            
+        } else {
+            
+            if (postcardImage) {
+                image(postcardImage, -cardW / 2, -cardH / 2, cardW, cardH);
+                    noFill();
+                    stroke(0);
+                    strokeWeight(4);
+                    rect(-cardW / 2, -cardH / 2, cardW, cardH);
+
+                    fill(0);
+                
+
+
+                textSize(10);
+                textWidth(postcardTitle);
+                let titleWidth = textWidth(postcardTitle);
+
+                if (titleWidth > cardW - 20) {
+                    textSize(8);
+                }
+
+                rect(-cardW / 2, -cardH / 2 + cardH - 20, titleWidth + 10, 20);
+
+                
+                fill(255);
+
+                text(postcardTitle, -cardW / 2 + 5, -cardH / 2 + cardH -5);
+                }
+            
+                
+
+                
+        }
+
+
+        pop();
+}
+
+
+
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw();
+}
+
+
+function mouseDragged() {
+    if (flipped && mouseX >= cardX && mouseX <= cardX + cardW && mouseY >= cardY && mouseY <= cardY + cardH) {
+        doodleLayer.stroke(0);
+        doodleLayer.strokeWeight(2);
+        doodleLayer.line(pmouseX - cardX, pmouseY - cardY, mouseX - cardX, mouseY - cardY);
+    }
+
+}
+
+async function sendPostcard() {
+    if (currentPostcardId) {
+        setDoc(doc(db, "postcards", currentPostcardId), {
+            imageTitle: postcardTitle,
+            imageUrl: postcardImageUrl,
+            doodle: doodleLayer.canvas.toDataURL(),
+            createdAt: Date.now()
+        });
+
+    } else {
+    addDoc(collection(db, "postcards"), {
+    imageTitle: postcardTitle,
+    imageUrl: postcardImageUrl,
+    doodle: doodleLayer.canvas.toDataURL(),
+    createdAt: Date.now()
+    });
+
+    };
+
+    postcardImage = null;
+    postcardImageUrl = "";
+    postcardTitle = "";
+    doodleLayer.background(255);
+    flipped = false;
+    cardRotation = 0;
+    targetRotation = 0;
+
+}
+
+async function receivePostcard() {
+                const snapshot = await getDocs(collection(db, "postcards"));
+                const postcards = snapshot.docs.map(docSnap => {
+            return {
+                id: docSnap.id,
+                ...docSnap.data()
+            };
+            });
+            if (postcards.length === 0) {
+            return;
+            }
+            const pickedPostcard = random(postcards);
+            currentPostcardId = pickedPostcard.id;
+            postcardTitle = pickedPostcard.imageTitle;
+            postcardImageUrl = pickedPostcard.imageUrl;
+            loadImage(postcardImageUrl, img => {
+            postcardImage = img;
+            });
+            doodleLayer.background(255);
+
+            if (pickedPostcard.doodle) {
+            loadImage(pickedPostcard.doodle, img => {
+                doodleLayer.image(img, 0, 0, cardW, cardH);
+            });
+            }
+}
+    
+
+
+
+window.setup = setup;
+window.draw = draw;
+window.windowResized = windowResized;
+
+window.mouseDragged = mouseDragged;
+//window.mousePressed = mousePressed;
+//window.mouseReleased = mouseReleased;
